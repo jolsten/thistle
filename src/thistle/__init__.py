377a6706bd388f447ef1dd3827625514facf8bfd
Jonathan Olsten <jolsten@gmail.com>
2025-03-30 10:20:12 -0400

    working on thistle cli to fix tle files and locate them

--- src/thistle/__init__.py
+++ src/thistle/__init__.py
@@ -1,6 +1,9 @@
-from importlib.metadata import version
+try:
+    from thistle._version import __version__
+except ImportError:
+    from importlib.metadata import version
 
-__version__ = version("thistle")
+    __version__ = version("thistle")
 
 from thistle.io import Loader, read_tle, read_tles, write_tle, write_tles
 from thistle.propagator import Propagator
